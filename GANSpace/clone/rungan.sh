148f6daa06a1ac0e321e5c1875bcafbe34e7f315
pikawika <lennertbontinck@live.be>
2021-04-04 21:48:29 +0200

    updated startup script to correct setting

--- GANSpace/clone/rungan.sh
+++ GANSpace/clone/rungan.sh
@@ -1,3 +1,3 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate ganspace
-python interactive.py --model=StyleGAN2 --class=car --layer=style -n=1_000_000 -b=10_000
+python interactive.py --model=StyleGAN2 --class=car --layer=style --creative_car_design=1 -n=1_000_000 -b=10_000
