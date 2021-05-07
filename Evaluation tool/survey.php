0576b3bc252d32ddd02ba87920dc460debafd1a3
pikawika <lennertbontinck@live.be>
2021-05-07 22:20:33 +0200

    Small typo fixed

--- Evaluation tool/survey.php
+++ Evaluation tool/survey.php
@@ -536,7 +536,7 @@ function show_single_photo_rating($image_id, $path, $user_knows_truth = false)
             <hr>
 
             <div class="form-group row">
-                <label class="col-sm-2 col-form-label">resemblance</label>
+                <label class="col-sm-2 col-form-label">Resemblance</label>
                 <div class="col-sm-10">
 
                     <div class="w-100">
